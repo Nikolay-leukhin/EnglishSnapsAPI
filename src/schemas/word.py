33ad2e3b65c2ac1476951adc843bc6842a20fa94
Nikolay-leukhin <83308245+Nikolay-leukhin@users.noreply.github.com>
2023-08-08 10:49:59 +0300

    Committing local changes before reverting

--- src/schemas/word.py
+++ src/schemas/word.py
@@ -11,15 +11,3 @@ class AddWordModel(BaseModel):
     def __repr__(self):
         return f"Word({self.name}, {self.theme_id}"
 
-
-
-class GetWordModel(BaseModel):
-    id: int
-    name: str
-    explanation: str
-    translation: str
-    transcription: str
-    theme_id: int
-
-    def __repr__(self):
-        return f"Word({self.id}, {self.name}, {self.theme_id}"
